55124a72505ba82e81755a1dfccf961665f1578a
Bram van Kooten <bram.vankooten@hotmail.com>
2023-11-20 14:59:43 +0100

    Add icons and sound to game.

--- game.js
+++ game.js
@@ -1,59 +1,178 @@
+screenHeight = window.innerHeight
+screenWidth = Math.min(window.innerWidth, Math.floor((screenHeight / 16) * 9))
+
+
 const game = new ex.Engine({
-    width: 600,
-    height: 800,
+    width: screenWidth,
+    height: screenHeight,
 })
+
+var loader = new ex.Loader();
+for (let i = 0; i < AnimalImages.length; i++){
+	loader.addResource(AnimalImages[i]);
+}
+for (let i = 0; i < FlowerImages.length; i++){
+	loader.addResource(FlowerImages[i]);
+}
+for (let i = 0; i < FruitImages.length; i++) {
+	loader.addResource(FruitImages[i]);
+}
+for (let i = 0; i < HouseImages.length; i++ ){
+	loader.addResource(HouseImages[i]);
+}
+for (let i = 0; i < ShapeImages.length; i++) {
+	loader.addResource(ShapeImages[i]);
+}
+for (var sound in Sounds) {
+    loader.addResource(Sounds[sound])
+}
 
 const screenPadding = 25
 
 function randomPosition() {
     let x = Math.floor(Math.random() * (game.drawWidth - 2*screenPadding) ) + screenPadding,
-        y = Math.floor(Math.random() * (game.drawHeight - 100) ) + 100;
+        y = Math.floor(Math.random() * (game.drawHeight - 200) ) + 100;
     return {x, y}
 }
 
-let round = 1
+function chooseIcons() {
+    choice = Math.floor(Math.random() * 5)
+
+    switch (choice) {
+        case 0:
+            return AnimalImages
+        case 1:
+            return FlowerImages
+        case 2:
+            return FruitImages
+        case 3:
+            return HouseImages
+        case 4:
+            return ShapeImages
+    }
+}
+
+let round = 20
 
 function newRound() {
+    icons = chooseIcons()
+
     let targetPos = randomPosition()
+    let targetIndex = Math.floor(Math.random() * icons.length)
 
     const target = new ex.Actor({
         x: targetPos.x,
         y: targetPos.y,
         height:50,
         width:50,
-        color: ex.Color.Red,
     })
 
+    sprite = new ex.Sprite({
+        image: icons[targetIndex],
+        destSize: {
+            height: 50,
+            width: 50,
+        }
+    })
+
+    target.graphics.use(sprite)
+
+    let iconsNoTarget = icons.slice()
+    iconsNoTarget.splice(targetIndex,1)
+    console.log(iconsNoTarget.length)
     
     game.add(target)
+    
+    const targetGraphic = new ex.Actor({
+        x: 130,
+        y: screenHeight - 35,
+        height:50,
+        width:50,
+    })
+    const targetGraphicBackground = new ex.Actor({
+        x: 130,
+        y: screenHeight - 35,
+        height:60,
+        width:60,
+        color: ex.Color.White
+    })
+    const targetLabel = new ex.Label({
+        text: 'Target:',
+        pos: ex.vec(5, screenHeight - 25),
+        font: new ex.Font({
+            family: 'impact',
+            size: 32,
+        }),
+    })
+
+    targetGraphic.graphics.use(sprite)
+
+    game.add(targetLabel)
+    game.add(targetGraphicBackground)
+    game.add(targetGraphic)
+    
 
     obstacles = []
 
     for (let i = 0; i < round*5; i++) {
         pos = randomPosition()
-        colors = [ex.Color.Blue, ex.Color.Orange, ex.Color.Yellow, ex.Color.Green]
-        randomChoice = Math.floor(Math.random() * colors.length)
+
+        distX = pos.x - targetPos.x
+        distY = pos.y - targetPos.y
+
+        if (Math.abs(distX) < 30 && Math.abs(distY) < 30) {
+            if (distX === 0) {
+                pos.x = pos.x + 30
+            }
+            else if (Math.abs(distX) < 30) {
+                pos.x = pos.x + Math.sign(distX)*30
+            }
+            
+            if (distY === 0) {
+                pos.y = pos.y + 30
+            }
+            else if (Math.abs(distY) < 30) {
+                pos.y = pos.y + Math.sign(distY)*30
+            }
+        }
+        
+        randomChoice = Math.floor(Math.random() * iconsNoTarget.length)
 
         obstacle = new ex.Actor({
             x: pos.x,
             y: pos.y,
             height: 50,
             width: 50,
-            color: colors[randomChoice]
         })
+
+        obstacleSprite = new ex.Sprite({
+            image: iconsNoTarget[randomChoice],
+            destSize: {
+                height: 50,
+                width: 50,
+            }
+        })
+        obstacle.graphics.use(obstacleSprite)
+
         obstacles.push(obstacle)
         game.add(obstacle)
     }
 
     target.on('pointerdown', function() {
-        round += 1
-        label.text = 'Round ' + round
         obstacles.forEach((obstacle) => {
             obstacle.kill()
         });
-        target.kill()
+        Sounds.correct.play(0.25)
+        target.actions.scaleBy(ex.vec(1.5,1.5), .75).callMethod(() => {
+            round += 1
+            label.text = 'Round ' + round
+            
+            target.kill()
+            targetGraphic.kill()
+            targetGraphicBackground.kill()
 
-        newRound()
+            newRound()
+        })
     })
 
     
@@ -87,4 +206,4 @@ newRound()
 
 // timer.start()
 
-game.start()
+game.start(loader)
---
+++ resources.js
@@ -0,0 +1,68 @@
+const AnimalImages = [
+    new ex.ImageSource('./Icons/Animal/Animal_10.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_eight.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_five.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_four.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_nine.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_one.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_seven.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_six.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_three.png'),
+    new ex.ImageSource('./Icons/Animal/Animal_two.png'),
+]
+
+const FlowerImages = [
+    new ex.ImageSource('./Icons/Flowers/Flower_one.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_two.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_three.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_four.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_five.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_six.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_seven.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_eight.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_nine.png'),
+    new ex.ImageSource('./Icons/Flowers/Flower_ten.png'),
+]
+
+const FruitImages = [
+    new ex.ImageSource('./Icons/Fruit/Fruit_one.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_two.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_three.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_four.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_five.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_six.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_seven.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_eight.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_nine.png'),
+    new ex.ImageSource('./Icons/Fruit/Fruit_ten.png'),
+]
+
+const HouseImages = [
+    new ex.ImageSource('./Icons/House/House_one.png'),
+    new ex.ImageSource('./Icons/House/House_two.png'),
+    new ex.ImageSource('./Icons/House/House_three.png'),
+    new ex.ImageSource('./Icons/House/House_four.png'),
+    new ex.ImageSource('./Icons/House/House_five.png'),
+    new ex.ImageSource('./Icons/House/House_six.png'),
+    new ex.ImageSource('./Icons/House/House_seven.png'),
+    new ex.ImageSource('./Icons/House/House_eight.png'),
+    new ex.ImageSource('./Icons/House/House_nine.png'),
+    new ex.ImageSource('./Icons/House/House_ten.png'),
+]
+
+const ShapeImages = [
+    new ex.ImageSource('./Icons/Shape/shape_one.png'),
+    new ex.ImageSource('./Icons/Shape/shape_two.png'),
+    new ex.ImageSource('./Icons/Shape/shape_three.png'),
+    new ex.ImageSource('./Icons/Shape/shape_four.png'),
+    new ex.ImageSource('./Icons/Shape/shape_five.png'),
+    new ex.ImageSource('./Icons/Shape/shape_six.png'),
+    new ex.ImageSource('./Icons/Shape/shape_seven.png'),
+    new ex.ImageSource('./Icons/Shape/Shape_eight.png'),
+    new ex.ImageSource('./Icons/Shape/shape_nine.png'),
+    new ex.ImageSource('./Icons/Shape/shape_ten.png'),
+]
+
+const Sounds = {
+    correct: new ex.Sound('./sounds/correct.mp3')
+}
